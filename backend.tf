553cf498910077b31fdadde9b262f308710e713c
BHUSHAN-GAJARE <gajare.bhushan46@gmail.com>
2024-01-16 22:13:15 +0530

    bucket name and folder created -main/terraform.tfstate

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraproject-01"
+    bucket = "young-minds-project01-bucket"
     key = "main/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table_01"
